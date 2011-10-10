8fd305d6f4e08862e12c0a8ef27a6127ef9214b2
wintergalt <devnull@localhost>
2011-10-09 21:11:17 -0300

    Improvements to the contacts frame (search label & text field)

--- src/main/groovy/com/vgs/pim/ui/contactsframe/ContactsFrame.groovy
+++ src/main/groovy/com/vgs/pim/ui/contactsframe/ContactsFrame.groovy
@@ -1,10 +1,20 @@
 package com.vgs.pim.ui.contactsframe
 
+import java.awt.BorderLayout
+import java.awt.FlowLayout
+
 import javax.swing.JInternalFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JTextField
+
+import com.vgs.pim.ui.util.UiUtils
 
 class ContactsFrame extends JInternalFrame {
 
 	private static ContactsFrame instance = null
+	private JPanel searchPanel = new JPanel(new FlowLayout())
+	private JTextField searchField = new JTextField(20)
 
 	private ContactsFrame() {
 	}
@@ -12,6 +22,15 @@ class ContactsFrame extends JInternalFrame {
 	private ContactsFrame(String title, boolean resizable, boolean closable,
 	boolean maximizable, boolean iconifiable) {
 		super(title, resizable, closable, maximizable, iconifiable)
+		setSize(400, 600)
+		setLocation(50, 50)
+		setTitle(UiUtils.getMessage("LABEL_CONTACTS"))
+		setLayout(new BorderLayout())
+		
+		searchPanel.add(new JLabel(UiUtils.getMessage("LABEL_SEARCH") + ":"))
+		searchPanel.add(searchField)
+		getContentPane().add(searchPanel,BorderLayout.NORTH)
+		instance
 	}
 
 	private ContactsFrame(String title, boolean resizable, boolean closable,
@@ -34,8 +53,6 @@ class ContactsFrame extends JInternalFrame {
 	static ContactsFrame getInstance(title=null) {
 		if(!instance) {
 			instance = new ContactsFrame(title, true, true, true, false)
-			instance.setSize(100, 100)
-			instance.setLocation(50, 50)
 		}
 		instance
 	}
